5f4e216a149d5beda34b085d6bed52d9755e7471
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-21 06:12:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let input=prompt("Give an array:");
+let arr= [];
+	input.split(',').forEach(val=>arr.push(parseInt(val)));
+let odd=[];
+let even=[];
+
+for(let i=0; i<arr.length; i++){
+	if(arr[i]%2==0){
+		even.push(arr[i]);
+	}
+	else if(arr[i]%2!=0){
+		odd.push(arr[i]);
+	}
+}
+const output=document.getElementById("output");
+setTimeout(()=>{
+	output.innerText=odd;
+}, 1000);
+setTimeout(()=>{
+	output.innerText=even;
+}, 2000)
