afbfb7444cf5601afe89f1a65154dbccd2fbc189
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-24 01:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ input = input.replace(/[\[\]\s]/g, ""); // Remove brackets and spaces
 
 // Split input string into an array of numbers
 let arr = input.split(',').map(val => parseInt(val, 10)); // Ensure elements are integers
+let even= arr.filter((val)=> val%2==0);
 
-// Get the output element
 const output = document.getElementById("output");
 
-// Filter and process arrays
-setTimeout(() => {
-    // Filter odd numbers
-    let odd = arr.filter(val => val % 2 !== 0);
-    // Display odd numbers after 1 second
-    output.innerText = `${odd.join(', ')}`;
-    
-    // Delay for 1 second before processing even number
-    setTimeout(() => {
-        // Multiply even numbers by 2
-        let even = arr.filter(val => val % 2 === 0).map(val => val * 2);
-        // Display even numbers after 2 seconds from the start
-        output.innerText += `${even.join(', ')}`;
-    }, 1000); // Delay 1 second for even numbers
-}, 1000);
+
+let promise=new Promise((resolve, reject)=>{
+	resolve(even);
+});
+promise
+.then((even)=>{
+	let mul= [];
+	setTimeout(()=>{
+		output.innerText=even;
+		even.forEach((val)=>mul.push(val*2));
+	},1000)
+	return mul;
+})
+.then((mul)=>{
+	setTimeout(()=>{
+		output.innerText=mul;
+	},2000)
+	})
+
+
