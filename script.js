1499d3900bcf02f52ce5c13dea78a8488702df5a
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-26 00:43:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,20 +11,13 @@ const output = document.getElementById("output");
 
 let promise=new Promise((resolve, reject)=>{
 setTimeout(() => {
-    let even = arr.filter(val => val % 2 == 0);
-    resolve(even);
-  }, 3000);
-});
-promise
-.then((even)=>{
-	return new Promise((resolve)=>{
-		setTimeout(()=>{
-		output.innerText=even.join(',');
+	let even=arr.filter((val)=>val%2==0);
+    output.innerText=even.join(',');
 		let mul=even.map(val => val * 2);
 		resolve(mul);
-	},1000)
+  }, 1000);
 });
-})
+promise
 .then((mul)=>{
 	return new Promise((resolve)=>{
 		setTimeout(()=>{
