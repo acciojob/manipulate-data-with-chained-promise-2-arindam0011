de33861b89ad4c62c33887ee8a37d21776174f73
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-26 00:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 let input = prompt("Give an array:");
 
 if(input){
-input = input.replace(/[\[\]\s]/g, "");
+    input = input.replace(/[\[\]\s]/g, "");
+    let arr = input.split(',').map(val => parseInt(val, 10)); 
+    const output = document.getElementById("output");
 
-let arr = input.split(',').map(val => parseInt(val, 10)); 
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let even = arr.filter((val) => val % 2 == 0);
+            output.innerText = even.join(',');
+            let mul = even.map(val => val * 2);
+            resolve(mul);
+        }, 1000);
+    });
 
-
-const output = document.getElementById("output");
-
-
-let promise=new Promise((resolve, reject)=>{
-setTimeout(() => {
-	let even=arr.filter((val)=>val%2==0);
-    output.innerText=even.join(',');
-		let mul=even.map(val => val * 2);
-		resolve(mul);
-  }, 1000);
-});
-promise
-.then((mul)=>{
-	return new Promise((resolve)=>{
-		setTimeout(()=>{
-		output.innerText=mul.join(',');
-			resolve();
-	},2000)
-	});
-})
+    promise.then((mul) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                output.innerText = mul.join(',');
+                resolve();
+            }, 2000)
+        });
+    })
 }
- 
